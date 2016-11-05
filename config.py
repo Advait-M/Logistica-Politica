34f04de98534502026368d95465fc88e607f2849
Advait Maybhate <advait.maybhate9@gmail.com>
2016-11-05 19:16:56 -0400

    Added config

---
+++ config.py
@@ -0,0 +1,11 @@
+consumer_key = "YxinjTAICC2NFXRrKd7CL1F9Y"
+consumer_secret = "Gyt50oM29u4c4wrQfY0qvgk9aeksdLBgaKMn3y2AdSdw1gqXcE"
+access_key = "4498748253-iYUMpczePTzMUwpZAH2DD3eILV7TkA70NUAUur3"
+access_secret = "UkOHKo8Q5RxsNznBECme2ygnmndQWFdhprnul6sad14cy"
+indico_key = "ffb6e651c89fbc4a0bbd6ab74d7d1c76"
+firebaseStuff = {
+  "apiKey": " AIzaSyAX2cDz-onhvkIUqlt57K3VV6W7g09g_ts ",
+  "authDomain": "https://logistica-politica.firebaseapp.com/",
+  "databaseURL": "https://logistica-politica.firebaseio.com/",
+  "storageBucket": "http://logistica-politica.appspot.com/"
+}
