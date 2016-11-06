2ece2d9ac11c080de535fa1afc417362eae0542a
Advait Maybhate <advait.maybhate9@gmail.com>
2016-11-06 09:58:01 -0500

    New error page

--- connectWeb.py
+++ connectWeb.py
@@ -13,33 +13,37 @@ def index():
         return render_template("index.html")
     else:
         data = pydb.LogiticaPolitica().getAll()
-        newUser = udc.UserData()
-        newUser.updateHandle(handle)
-        tweets = twitData.getTweets(handle)
-        newUser.updateRealName(tweets[0])
-        newUser.updateOpinion(tweets[1])
-        newUser.updateMood(newUser.askInfo("mood", "string"))
-        curMood = newUser.mood
-        curMood = curMood.title()
-        newUser.updateParty(newUser.askInfo("party", "string"))
-        newUser.addToDB()
-        eStuff = newUser.compareWithParty()
-        amountError = eStuff[0]
-        amountError = round(amountError*100,2)
-        emotionsParty = eStuff[1]
-        interpretError = newUser.interpretError(amountError)
-        partyConfidence = newUser.percentParty()
-        print(partyConfidence)
-        #Important interface
-        # return str((newUser.twitterHandle, newUser.realName, newUser.opinionString, newUser.mood, newUser.politicalParty))
-        # listOf(dictOf(Dict) Float listOf(float) Str  Str Str Str
-        return render_template("results.html", data = data, amountError = amountError, emotionsParty = emotionsParty, interpretError = interpretError, handle = newUser.twitterHandle, name = newUser.realName, tweet = newUser.opinionString, mood = curMood, party = newUser.politicalParty, partyConfidence = partyConfidence)
+        if data != []:
+            newUser = udc.UserData()
+            newUser.updateHandle(handle)
+            tweets = twitData.getTweets(handle)
+            newUser.updateRealName(tweets[0])
+            newUser.updateOpinion(tweets[1])
+            newUser.updateMood(newUser.askInfo("mood", "string"))
+            curMood = newUser.mood
+            curMood = curMood.title()
+            newUser.updateParty(newUser.askInfo("party", "string"))
+            newUser.addToDB()
+            eStuff = newUser.compareWithParty()
+            amountError = eStuff[0]
+            amountError = round(amountError*100,2)
+            emotionsParty = eStuff[1]
+            interpretError = newUser.interpretError(amountError)
+            partyConfidence = newUser.percentParty()
+            print(partyConfidence)
+            #Important interface
+            # return str((newUser.twitterHandle, newUser.realName, newUser.opinionString, newUser.mood, newUser.politicalParty))
+            # listOf(dictOf(Dict) Float listOf(float) Str  Str Str Str
+            return render_template("results.html", data = data, amountError = amountError, emotionsParty = emotionsParty, interpretError = interpretError, handle = newUser.twitterHandle, name = newUser.realName, tweet = newUser.opinionString, mood = curMood, party = newUser.politicalParty, partyConfidence = partyConfidence)
 @app.route("/howitworks.html")
 def showPage():
     return render_template("howitworks.html")
 @app.route("/index.html")
 def showHome():
     return(render_template("index.html"))
+@app.errorhandler(500)
+def  internalError(e):
+    return(render_template("error500.html"))
 if __name__ == "__main__":
     pydb.LogiticaPolitica().start()
     app.run()
