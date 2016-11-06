776426d8e47c221261146f59887f44d974d6fb60
Misha Kuzma <mishakuzma@gmail.com>
2016-11-06 01:38:58 -0400

    Changed error to float point

--- userDataClass.py
+++ userDataClass.py
@@ -132,11 +132,11 @@ class UserData:
         for i in range(0, len(emotionsOfUserParty)):
             emotionsOfUserParty[i] = emotionsOfUserParty[i]/partyCount
 
-        error = 0
+        error = 0.0
         userD = self.askInfo("mood", "dictionary")
         userMood = [userD["anger"], userD["joy"], userD["sadness"], userD["surprise"], userD["fear"]]
         for i in range(0,len(emotionsOfUserParty)):
-            tempError = ((userMood[i]-emotionsOfUserParty[i]) ** 2)**(1/2)
+            tempError = ((userMood[i] - emotionsOfUserParty[i]) ** 2)**(1/2)
             error += tempError
 
         return [error, emotionsOfUserParty]
