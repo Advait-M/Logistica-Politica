b1345d488e8aee81ced518a3710a5234f2a9f061
Advait Maybhate <advait.maybhate9@gmail.com>
2016-11-05 23:21:48 -0400

    form stuff?
    oug

--- connectWeb.py
+++ connectWeb.py
@@ -1,13 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pyredb as pydb
+import userDataClass as udc
 
 app = Flask(__name__)
 @app.route("/")
 def index():
-    print("a")
-    data = pydb.LogiticaPolitica().getAll()
-    print(data)
-    return render_template("index.html", data = data)
+	handle = request.args.get("handle")
+	if handle == None:
+		
+		return render_template("index.html")
+	else:
+		data = pydb.LogiticaPolitica().getAll()
+		newUser = udc.UserData()
+		newUser.updateHandle(handle)
+        newUser.updateRealName(tweets[0])
+        newUser.updateOpinion(twitData.getTweets(i)[1])
+        newUser.updateMood(newUser.askInfo("mood", "string"))
+        newUser.updateParty(newUser.askInfo("party", "string"))
+        return str((newUser.handle, newUser.name, newUser.opinion, newUser.mood, newUser.party))
+        newUser.addToDB()
+		return render_template("results.html", handle = newUser.handle, name = newUser.name, tweet = newUser.opinion, mood = newUser.mood, party = newUser.party)
 
 if __name__ == "__main__":
     pydb.LogiticaPolitica().start()
